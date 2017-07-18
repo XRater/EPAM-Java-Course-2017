e6507c949a2493a2dba8dab8a9a69b3954506a9a
XRater <kair1998@gmail.com>
2017-07-19 01:22:24 +0300

    BasicJava StringStack: basic test compiled

--- BasicJava/src/HomeWork/p120717/StackTests.java
+++ BasicJava/src/HomeWork/p120717/StackTests.java
@@ -1,6 +1,41 @@
 package HomeWork.p120717;
 
+import TestFramework.Asserts;
+
 public class StackTests {
 
+    public static void main(String[] args) {
+
+        test_basic();
+        test_push();
+        test_pop();
+        test_sizes();
+
+    }
+
+    private static void test_basic() {
+        StringStack stack = new StringStack();
+
+        Asserts.assertEquals(stack.toString(), "[]");
+        Asserts.assertEquals(stack.isEmpty(), true);
+        Asserts.assertEquals(stack.getSize(), 0);
+
+        stack.push("one");
+        stack.push("two");
+        stack.push("three");
+
+        Asserts.assertEquals(stack.toString(), "[one, two, three]");
+        Asserts.assertEquals(stack.isEmpty(), false);
+        Asserts.assertEquals(stack.getSize(), 3);
+    }
+
+    private static void test_push() {
+    }
+
+    private static void test_pop() {
+    }
+
+    private static void test_sizes() {
+    }
 
 }
--- BasicJava/src/HomeWork/p120717/StringStack.java
+++ BasicJava/src/HomeWork/p120717/StringStack.java
@@ -3,4 +3,15 @@ package HomeWork.p120717;
 public class StringStack {
 
 
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int getSize() {
+        return 0;
+    }
+
+    public void push(String s) {
+
+    }
 }
