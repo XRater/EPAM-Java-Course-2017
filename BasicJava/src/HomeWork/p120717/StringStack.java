5b2cd147a8372363bd0e43141aef1c7831cce836
XRater <kair1998@gmail.com>
2017-07-19 01:56:20 +0300

    BasicJava StringStack: Constructor isEmpty getSize

--- BasicJava/src/HomeWork/p120717/StringStack.java
+++ BasicJava/src/HomeWork/p120717/StringStack.java
@@ -2,16 +2,33 @@ package HomeWork.p120717;
 
 public class StringStack {
 
+    private static final int DEFAULT_MAX_SIZE = 10;
+
+    private final int maxSize;
+    private int size_;
+
+    String[] data_;
+
+    public StringStack() {
+        maxSize = DEFAULT_MAX_SIZE;
+        data_ = new String[maxSize];
+        size_ = 0;
+    }
 
     public boolean isEmpty() {
-        return false;
+        return size_ == 0;
     }
 
     public int getSize() {
-        return 0;
+        return size_;
     }
 
     public void push(String s) {
 
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
