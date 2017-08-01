f81ff8309bbdbe7cb1a2f63dd8a33dfd58d91017
XRater <kair1998@gmail.com>
2017-08-01 15:41:11 +0300

    HackerRank Day18

--- BasicJava/src/HomeWork/hackerrank/Day18.java
+++ BasicJava/src/HomeWork/hackerrank/Day18.java
@@ -1,6 +1,16 @@
 package HomeWork.hackerrank;
 
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 class Day18 {
 
+    public static void main(String[] args) {
+
+        Queue<Character> q = new LinkedList<>();
+        Stack<Character> s = new Stack<>();
+
+    }
 
 }
